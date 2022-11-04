e2be7132ef98c1a6677df5b55622e173983ee387
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-11-04 18:49:41 +0700

    Add darkmode on HTML

--- js/background.js
+++ js/background.js
@@ -1,10 +1,8 @@
-const bgList =[
+const bgList_light =[
     "1.jpeg",
     "2.png",
     "3.jpg",
     "4.jpg",
-    "5.png",
-    "6.jpg",
     "7.png",
     "8.jpeg",
     "9.jpeg",
@@ -12,8 +10,34 @@ const bgList =[
     "11.jpeg",
 ];
 
-const chosenImg = bgList[Math.floor(Math.random()*bgList.length)];
+const bgList_dark=[
+    "5.png",
+    "6.jpg",
+];
+
+const chosenImg_light = bgList_light[Math.floor(Math.random()*bgList_light.length)];
+const chosenImg_dark = bgList_dark[Math.floor(Math.random()*bgList_dark.length)];
+
 const bgImage = document.createElement("img");
 
-bgImage.src =`img/${chosenImg}`;
+bgImage.src =`img/${chosenImg_light}`;
 document.body.appendChild(bgImage);
+
+// 다크모드 추가
+const body = document.querySelector('body');
+const darkBtn = document.querySelector('.darkBtn');
+const lightBtn = document.querySelector('.lightBtn');
+const DARKBTN_CLASS='dark_mode';
+
+lightBtn.addEventListener('click',()=>{
+
+document.body.classList.remove(DARKBTN_CLASS);
+
+})
+
+
+darkBtn.addEventListener('click',()=>{
+
+    document.body.classList.add(DARKBTN_CLASS);
+    
+})
